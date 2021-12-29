2fc6ba0be6e15eac86ed2411d9fb35b87e44a4f7
Fufu <fufuok@qq.com>
2021-12-29 08:44:56 +0800

    refactor(cpu)!: reduce loadavg alarm priority

--- src/aggs/cpu.py
+++ src/aggs/cpu.py
@@ -4,7 +4,7 @@
     ~~~~~~~~
     CPU 数据汇聚/报警插件
 
-    :author: Fufu, 2021/11/17 报警优先级: 系统 1 分钟内负载 > CPU 总使用率 > CPU 单核使用率
+    :author: Fufu, 2021/11/17 报警优先级: CPU 总使用率 > CPU 单核使用率 > 系统 1 分钟内负载
 """
 from . import AggsPlugin
 from ..libs.metric import Metric
@@ -19,12 +19,6 @@ class Cpu(AggsPlugin):
         """CPU 报警"""
         alarm_conf = self.get_plugin_conf_value('alarm', {})
         if not alarm_conf:
-            return metric
-
-        loadavg_precent_1 = metric.get('loadavg_precent_1', -0.1)
-        conf_loadavg_precent_1 = self.get_plugin_conf_value('alarm|loadavg_precent_1', -0.1)
-        if loadavg_precent_1 >= conf_loadavg_precent_1 >= 0.0:
-            self.put_alarm_metric(f'系统平均负载过高(%): {loadavg_precent_1}>={conf_loadavg_precent_1}')
             return metric
 
         more = ''
@@ -43,6 +37,12 @@ class Cpu(AggsPlugin):
         conf_max_percent = self.get_plugin_conf_value('alarm|max_percent', -0.1)
         if max_percent >= conf_max_percent >= 0.0:
             self.put_alarm_metric(f'CPU 单核使用率过高(%): {max_percent}>={conf_max_percent}', data=data, more=more)
+            return metric
+
+        loadavg_precent_1 = metric.get('loadavg_precent_1', -0.1)
+        conf_loadavg_precent_1 = self.get_plugin_conf_value('alarm|loadavg_precent_1', -0.1)
+        if loadavg_precent_1 >= conf_loadavg_precent_1 >= 0.0:
+            self.put_alarm_metric(f'系统平均负载过高(%): {loadavg_precent_1}>={conf_loadavg_precent_1}')
             return metric
 
         return metric
