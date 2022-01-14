f04b0d05e733b9989562934f2cdf1fa7a6af947a
Fufu <fufuok@qq.com>
2022-01-14 08:56:25 +0800

    chore: bump v0.2.6

--- src/app.py
+++ src/app.py
@@ -68,7 +68,7 @@ class Worker:
 
 async def main() -> None:
     """程序入口"""
-    logger.info('PyAgent(v0.2.5.21122417) start working')
+    logger.info('PyAgent(v0.2.6.22011010) start working')
 
     while True:
         await CONF.update()
